4556ecf0262e5fbacc38a90af0b756277a3f3408
Said Thaher <saidthaher@Saids-MacBook-Pro.local>
2015-10-01 09:01:51 +0200

    new update

--- app/js/addPost.js
+++ app/js/addPost.js
@@ -7,61 +7,15 @@ angular.module('myApp.addPost', ['ngRoute'])
     $location.path('/main');
 	}
 
-	$scope.upload = function(files){
-
-		        if (files && files.length) {
-		        for (var i = 0; i < files.length; i++) {
-		        	
-            	console.log(files);
-         		$scope.upload = Upload.dataUrl(files, disallowObjectUrl).then(function(url){
-        		//console.log(url);
-
-        	});
-
-         		file.upload.then(function (response) {
-      				$timeout(function () {
-       				 file.result = response.data;
-   				   });
-   				 }, function (response) {
-     				 if (response.status > 0)
-      				  console.log(response.status + ': ' + response.data);
-    				});
-         	}
-    	}
-
-	}
 
 	//Add data to firebase
 	$scope.AddPost = function(files) {
 			var fb = new Firebase("https://hotelboard.firebaseio.com/Articles/");
-			/*
-			$scope.files = files;
-		        if (files && files.length > 0) {
-		        for (var i = 0; i < files.length; i++) {
-		        	var file = files[i];
-            	console.log(file);
-         		file.upload = Upload.dataUrl(file, disallowObjectUrl).then(function(url){
-        		//console.log(url);
-        	});
-
-         		file.upload.then(function (response) {
-      				$timeout(function () {
-       				 file.result = response.data;
-   				   });
-   				 }, function (response) {
-     				 if (response.status > 0)
-      				  console.log(response.status + ': ' + response.data);
-    				});
-         	}
-    	}*/
-
-
-		
-
+			
 			var title = $scope.article.title;
 			var post  = $scope.article.post;
 			var user  = CommonProp.getUser();
-			var images = $scope.upload(files);
+			var images = $scope.file ;
 			fb.push({
 				title:     title,
 				post:      post,
@@ -74,6 +28,7 @@ angular.module('myApp.addPost', ['ngRoute'])
 					console.log("Error:",error);
 				} else {
 				console.log("Post set successfully!");
+				console.log($scope.file);
 				$location.path('/home');
 				$scope.$apply();
 			
